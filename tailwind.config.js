26acc0b3d248f2b155ded4be99ba17dc3fb0a1e6
SergShcherbina <1990sergik27@gmail.com>
2023-11-26 23:06:18 +0300

    add tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: ["./src/**/*.{js,jsx,ts,tsx}"],
+    theme: {
+        extend: {
+            colors: {
+                main: '#6b8afd',
+                first: '#121212',
+                second: '#2e333d',
+            },
+        },
+    },
+    plugins: [],
 }
 
