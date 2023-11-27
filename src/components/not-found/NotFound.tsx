b8a0427bdc2639f4cd912f58983a1637577d3e6d
SergShcherbina <1990sergik27@gmail.com>
2023-11-27 22:32:14 +0300

    fix NotFound.tsx

--- src/components/not-found/NotFound.tsx
+++ src/components/not-found/NotFound.tsx
@@ -10,7 +10,7 @@ export const NotFound = () => {
                 <div className="mt-10 flex items-center justify-center gap-x-6">
                     <Link
                         to="/"
-                        className="rounded-md border border-orange-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-orange-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Go
+                        className="rounded-md border border-orange-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-orange-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-orange-500">Go
                         back home
                     </Link>
                 </div>
